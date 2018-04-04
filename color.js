000a42c6ae7d3844766f02ec58dd6ae8217535e7
Ronny Gutierrez <ronny.icne@gmail.com>
2018-04-03 19:45:01 -0700

    update 7:41pm
    Made a function called changeColors that makes all squares turn into the same color as the one that is correct.
    This function is put in the if statement  of clickedColor === pickedColor so it can activate once the correct square is clicked.

--- color.js
+++ color.js
@@ -19,14 +19,21 @@ for(var i = 0; i < squares.length; i++){
     
     //add click listeners to squares
     squares[i].addEventListener("click", function(){
+        debugger;
         var clickedColor = this.style.backgroundColor;
         if(clickedColor === pickedColor){
          messageDisplay.textContent = "Correct!";
-            
+            changeColors(clickedColor);
         }else{
              this.style.background = "#232323";
              messageDisplay.textContent = "Try Again";
             }
        
     });
+}
+
+function changeColors(color){
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
 }
